39f79b290d434d9305a2448e2afa58325ba2a192
Aziza Novruzova <mamedova.aziza@gmail.com>
2020-01-09 11:41:44 -0500

    My first commit to github

---
+++ calculator.js
@@ -0,0 +1,37 @@
+//jshint esversion:6
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/bmiCalculator", function(req, res){
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmiCalculator", function(req, res){
+  var weight = Number(req.body.weight);
+  var height = Number(req.body.height);
+
+  var bmi = weight / (height * height);
+
+  res.send("BMI is " + bmi);
+})
+
+app.post("/", function(req, res){
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+
+  var result = num1 + num2;
+  res.send("<h3>The result is " + result + " </h3>");
+
+});
+
+app.listen("3000", function(){
+  console.log("Server is running now.");
+});
